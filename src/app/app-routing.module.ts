750e416e49f76c597adba32fbebf2269e7e03caa
yxomar <paulinoomar22@gmail.com>
2023-08-24 15:23:57 +0200

    1 end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path:"header",component:HeaderComponent},
+  {path: "footer", component: FooterComponent},
+  {path: "form-register", component: FormRegisterComponent},
+  {path: "register", component: FormRegisterComponent},
+  {path: "profile", component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
